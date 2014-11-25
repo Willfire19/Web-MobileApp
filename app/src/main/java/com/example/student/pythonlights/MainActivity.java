049525c589888622a76beac9d1a2b9009d93f9aa
ama4tf <liddy260022>
2014-11-25 15:43:37 -0500

    Merged Patrick's changes

--- app/src/main/java/com/example/student/pythonlights/MainActivity.java
+++ app/src/main/java/com/example/student/pythonlights/MainActivity.java
@@ -22,6 +22,7 @@ import android.location.*;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 import com.google.android.gms.common.ConnectionResult;
